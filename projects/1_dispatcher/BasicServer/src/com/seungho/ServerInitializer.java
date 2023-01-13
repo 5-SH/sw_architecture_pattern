b558facd912ea6c9d0b393ce5c804ae109616fcb
5-SH <llll_lll_lll@naver.com>
2023-01-13 14:12:31 +0900

    테스트 코드 수정

--- projects/1_dispatcher/BasicServer/src/com/seungho/ServerInitializer.java
+++ projects/1_dispatcher/BasicServer/src/com/seungho/ServerInitializer.java
@@ -9,21 +9,13 @@ import java.net.Socket;
 public class ServerInitializer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int port = 5000;
 		System.out.println("Server ON : " + port);
 		
 		try {
 			ServerSocket serverSocket = new ServerSocket(port);
 			Dispatcher dispatcher = new Dispatcher();
-//			Socket connection;
 			while (true) {
-//				connection = serverSocket.accept(); // Dispatcher
-//				InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-//				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-//				String line = bufferedReader.readLine();
-//				System.out.println("READ:" + line);
-				
 				dispatcher.dispatch(serverSocket);
 			}
 			
--- projects/1_dispatcher/BasicServer/src/com/seungho/TestClient.java
+++ projects/1_dispatcher/BasicServer/src/com/seungho/TestClient.java
@@ -10,22 +10,27 @@ public class TestClient {
 		System.out.println("Client ON");
 		
 		try {
-			String message;
-			
-			Socket socket = new Socket("127.0.0.1", 5000);
-			OutputStream out = socket.getOutputStream();
-			message = "0x5001|홍길동|22";
-			out.write(message.getBytes());
-			socket.close();
-				
-			Socket socket2 = new Socket("127.0.0.1", 5000);
-			OutputStream out2 = socket2.getOutputStream();
-			message = "0x6001|hong|1234|홍길동|22|남성";
-			out2.write(message.getBytes());
-			socket2.close();
+			// Server 에 telnet 으로 추가 접속하면 telnet 이 메시지를 보내기 전까지 블로킹 된다.
+			while (true) {
+				String message;
+
+				Socket socket = new Socket("127.0.0.1", 5000);
+				OutputStream out = socket.getOutputStream();
+				message = "0x5001|홍길동|22";
+				out.write(message.getBytes());
+				socket.close();
+
+				Socket socket2 = new Socket("127.0.0.1", 5000);
+				OutputStream out2 = socket2.getOutputStream();
+				message = "0x6001|hong|1234|홍길동|22|남성";
+				out2.write(message.getBytes());
+				socket2.close();
+
+				Thread.sleep(1000);
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
